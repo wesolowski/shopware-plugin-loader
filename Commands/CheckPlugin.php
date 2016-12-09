a9d1ed4f393353033b5a7909a8a02dd0011afd6a
Rafal Wesolowski <wesolowski@nexus-netsoft.com>
2016-12-09 23:41:58 +0100

    check plugin

--- Commands/CheckPlugin.php
+++ Commands/CheckPlugin.php
@@ -2,6 +2,7 @@
 namespace RawPluginLoader\Commands;
 
 use Shopware\Commands\ShopwareCommand;
+use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
@@ -11,7 +12,7 @@ class CheckPlugin extends ShopwareCommand
     protected function configure()
     {
         $this
-            ->setName('raw:plugin-loader:check')
+            ->setName('raw:plugin-loader')
             ->setDescription('activate/deactivate shopware plugins');
     }
 
@@ -24,6 +25,63 @@ class CheckPlugin extends ShopwareCommand
     {
         $output->writeln("Start");
 
+        $pluginsConfigs = $this->getPluginConfigs();
+        $pluginListInfo = $this->getShopwarePluginInfo();
+
+        foreach ($pluginsConfigs as $pluginPath => $pluginConfig) {
+
+        }
+
         $output->writeln("CheckPlugin is completed");
+    }
+
+    /**
+     * @return array
+     */
+    protected function getPluginConfigs()
+    {
+        $shopDir = $this->getContainer()->get('kernel')->getRootDir();
+        $pluginInfo = [];
+
+        $pluginDistConfigs = glob($shopDir . "/custom/plugins/*/pluginconfig.dist.php");
+        foreach ($pluginDistConfigs as $pluginDistConfig) {
+            $pluginInfo[dirname($pluginDistConfig)] = require $pluginDistConfig;
+        }
+
+        $pluginConfigs = glob($shopDir . "/custom/plugins/*/pluginconfig.php");
+        foreach ($pluginConfigs as $pluginConfig) {
+            $dirPluginConfig = dirname($pluginConfig);
+            $pluginConfigInfo = require $pluginConfig;
+            $pluginInfo[dirname($pluginConfig)] = (isset($pluginInfo[$dirPluginConfig]))
+                ? array_merge($pluginInfo[$dirPluginConfig], $pluginConfigInfo)
+                : $pluginConfigInfo;
+        }
+        return $pluginInfo;
+    }
+
+    /**
+     * @return array
+     */
+    protected function getShopwarePluginInfo()
+    {
+        /** @var ModelManager $em */
+        $em = $this->container->get('models');
+
+        $repository = $em->getRepository('Shopware\Models\Plugin\Plugin');
+        $builder = $repository->createQueryBuilder('plugin');
+        $builder->andWhere('plugin.capabilityEnable = true');
+        $builder->addOrderBy('plugin.name');
+        $plugins = $builder->getQuery()->execute();
+
+        $rows = [];
+
+        /** @var Plugin $plugin */
+        foreach ($plugins as $plugin) {
+            $rows[$plugin->getName()] = [
+                'active' => (bool)$plugin->getActive(),
+                'installed' => (bool)$plugin->getInstalled()
+            ];
+        }
+        return $rows;
     }
 }
