06a0734f8ccbcc5bdde40e71bd331223573909d6
Rafal Wesolowski <wesolowski@nexus-netsoft.com>
2016-12-10 00:45:24 +0100

    Add activate and unistall plugin

--- Commands/CheckPlugin.php
+++ Commands/CheckPlugin.php
@@ -25,13 +25,39 @@ class CheckPlugin extends ShopwareCommand
     {
         $output->writeln("Start");
 
+        $app = $this->getApplication();
+        $app->doRun(new ArrayInput([
+            'command' => 'sw:plugin:refresh',
+        ]), $output);
+
         $pluginsConfigs = $this->getPluginConfigs();
         $pluginListInfo = $this->getShopwarePluginInfo();
 
+        $output->writeln("");
         foreach ($pluginsConfigs as $pluginPath => $pluginConfig) {
+            $pluginIndent = basename($pluginPath);
 
+            if (isset($pluginConfig['active'])) {
+                $isPluginActive = (bool)$pluginConfig['active'];
+
+                if (isset($pluginListInfo[$pluginIndent])
+                    && !$pluginListInfo[$pluginIndent]['active']
+                    && $isPluginActive
+                ) {
+                    $this->activatePlugin($output, $pluginIndent);
+                } elseif (isset($pluginListInfo[$pluginIndent])
+                    && $pluginListInfo[$pluginIndent]['active']
+                    && !$isPluginActive
+                ) {
+                    $this->unistallPlugin($output, $pluginIndent);
+                }
+
+                if (!$isPluginActive) {
+                    $this->deleteUnitTestFolder($pluginPath);
+                }
+            }
         }
-
+        $output->writeln("");
         $output->writeln("CheckPlugin is completed");
     }
 
@@ -83,5 +109,61 @@ class CheckPlugin extends ShopwareCommand
             ];
         }
         return $rows;
+    }
+
+    /**
+     * @param OutputInterface $output
+     * @param $pluginIndent
+     * @return array
+     */
+    private function activatePlugin(OutputInterface $output, $pluginIndent)
+    {
+        $app = $this->getApplication();
+
+        $input = new ArrayInput([
+            'command' => 'sw:plugin:install',
+            'plugin' => $pluginIndent
+        ]);
+        $app->doRun($input, $output);
+
+        $input = new ArrayInput([
+            'command' => 'sw:plugin:activate',
+            'plugin' => $pluginIndent
+        ]);
+        $app->doRun($input, $output);
+    }
+
+    /**
+     * @param OutputInterface $output
+     * @param $pluginIndent
+     */
+    protected function unistallPlugin(OutputInterface $output, $pluginIndent)
+    {
+        $app = $this->getApplication();
+
+        $input = new ArrayInput([
+            'command' => 'sw:plugin:deactivate',
+            'plugin' => $pluginIndent
+        ]);
+        $app->doRun($input, $output);
+
+        $input = new ArrayInput([
+            'command' => 'sw:plugin:uninstall',
+            'plugin' => $pluginIndent
+        ]);
+        $app->doRun($input, $output);
+    }
+
+    /**
+     * @param $pluginPath
+     */
+    private function deleteUnitTestFolder($pluginPath)
+    {
+        system("rm -rf " . escapeshellarg($pluginPath . '/Tests'));
+        system("rm -rf " . escapeshellarg($pluginPath . '/Test'));
+        system("rm -rf " . escapeshellarg($pluginPath . '/test'));
+        system("rm -rf " . escapeshellarg($pluginPath . '/tests'));
+        system("rm -rf " . escapeshellarg($pluginPath . '/phpunit.xml'));
+        system("rm -rf " . escapeshellarg($pluginPath . '/phpunit.xml.dist'));
     }
 }
